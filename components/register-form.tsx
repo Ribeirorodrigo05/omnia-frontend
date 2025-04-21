60d67c52b2cb7b1bd45fc0fa9dc74311ac274dfa
Ribeirorodrigo05 <Ribeiro.rodrigo05@gmail.com>
2025-04-21 11:40:07 -0300

    add shadcn ui and register form validation

---
+++ components/register-form.tsx
@@ -0,0 +1,73 @@
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+import Logo from "@/public/omnia.png";
+
+import Image from "next/image";
+
+export function RegisterForm({
+  className,
+  ...props
+}: React.ComponentProps<"div">) {
+  return (
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <Card>
+        <CardHeader>
+          <div className="flex items-center justify-center">
+            <Image
+              src={Logo}
+              alt="Logo"
+              width={180}
+              height={120}
+              className="mb-4"
+            />
+          </div>
+          <CardTitle>Create an account</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="flex flex-col gap-6">
+              <div className="grid gap-3">
+                <Label htmlFor="name">Name</Label>
+                <Input id="name" type="text" placeholder="m@example.com" />
+              </div>
+              <div className="grid gap-3">
+                <Label htmlFor="email">Email</Label>
+                <Input id="email" type="email" placeholder="m@example.com" />
+              </div>
+              <div className="grid gap-3">
+                <div className="flex items-center">
+                  <Label htmlFor="password">Password</Label>
+                </div>
+                <Input id="password" type="password" required />
+              </div>
+              <div className="grid gap-3">
+                <div className="flex items-center">
+                  <Label htmlFor="password">Confirm password</Label>
+                </div>
+                <Input id="confirmPassword" type="password" required />
+              </div>
+              <div className="flex flex-col gap-3">
+                <Button type="submit" className="w-full">
+                  Login
+                </Button>
+                <Button variant="outline" className="w-full">
+                  Login with Google
+                </Button>
+              </div>
+            </div>
+            <div className="mt-4 text-center text-sm">
+              Don&apos;t have an account?{" "}
+              <a href="/login" className="underline underline-offset-4">
+                Sign up
+              </a>
+            </div>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
---
+++ middleware.ts
@@ -0,0 +1,44 @@
+import { NextResponse, type NextRequest } from "next/server";
+
+const publicRoutes = [
+  {
+    path: "/login",
+  },
+  {
+    path: "/sign-up",
+  },
+];
+
+export function middleware(request: NextRequest) {
+  console.log("Middleware triggered for path:", request.nextUrl.pathname);
+  const isPublicRoute = publicRoutes.find(
+    (route) => request.nextUrl.pathname === route.path
+  );
+
+  if (isPublicRoute) {
+    return NextResponse.next();
+  }
+
+  const token = request.cookies.get("token")?.value;
+
+  if (!token) {
+    const url = request.nextUrl.clone();
+    url.pathname = "/login";
+    return NextResponse.redirect(url);
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico, sitemap.xml, robots.txt (metadata files)
+     */
+    "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)",
+  ],
+};
