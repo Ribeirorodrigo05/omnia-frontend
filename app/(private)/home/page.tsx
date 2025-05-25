cf83fad66c28a8aac199df5b0a944772e1334a3e
Ribeirorodrigo05 <Ribeiro.rodrigo05@gmail.com>
2025-05-25 20:50:13 -0300

    removing all code

--- app/(private)/home/page.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-import { userAtom } from "@/atoms/user";
-import { useAtom } from "jotai";
-
-export default function HomePage() {
-  const [user] = useAtom(userAtom);
-  return <div>{JSON.stringify(user)}</div>;
-}
--- app/api/repositories/auth.ts
+++
@@ -1,9 +0,0 @@
-import { fetchData } from "../fetchprovider";
-
-export async function authenticateUser(email: string, password: string) {
-  const endpoint = "/authentication/login";
-
-  const response = await fetchData(endpoint, { email, password }, "POST");
-
-  return response;
-}
